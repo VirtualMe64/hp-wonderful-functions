8d7849ed270fb88fafb309cf4315feb7c5ba6ef5
Sammy Taubman <staubman1@gmail.com>
2025-03-29 23:24:27 -0400

    Add more test data

--- mockdata/uploadDoorDashData.py
+++ mockdata/uploadDoorDashData.py
@@ -7,23 +7,24 @@ creds = credentials.Certificate("./service-account.json")
 app = initialize_app(creds)
 fsdb = firestore.client(app)
 
-path = "./doordash.txt"
-with open(path, "r") as file:
-    data = json.load(file)
+for merchant in ["DoorDash", "Instacart"]:
+    path = f"./{merchant.lower()}.json"
+    with open(path, "r") as file:
+        data = json.load(file)
 
-for transaction in data['transactions']:
-    fs_transaction = {
-        "merchant": "DoorDash",
-        "datetime": datetime.strptime(transaction["datetime"], "%Y-%m-%dT%H:%M:%S%z"),
-        "status": transaction["order_status"],
-        "products": [
-            {
-                "id": product["external_id"],
-                "name": product["name"],
-                "price": product["price"]["total"]
-            } for product in transaction["products"]
-        ]
-    }
-    fs_transaction["location"] = [40.351978, -74.655198]
-    key = transaction["id"]
-    fsdb.collection("transactions").document(str(key)).set(fs_transaction)
+    for transaction in data['transactions']:
+        fs_transaction = {
+            "merchant": merchant,
+            "datetime": datetime.strptime(transaction["datetime"], "%Y-%m-%dT%H:%M:%S%z"),
+            "status": transaction["order_status"],
+            "products": [
+                {
+                    "id": product["external_id"],
+                    "name": product["name"],
+                    "price": product["price"]["total"]
+                } for product in transaction["products"]
+            ]
+        }
+        fs_transaction["location"] = [40.351978, -74.655198]
+        key = transaction["id"]
+        fsdb.collection("transactions").document(str(key)).set(fs_transaction)
